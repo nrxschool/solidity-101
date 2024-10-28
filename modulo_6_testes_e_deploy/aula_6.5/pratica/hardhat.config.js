d99fa2cb61f922c85d0c4b90639e205d47682a1a
viniciushrcs <viniciushrcs@gmail.com>
2024-10-28 19:23:51 -0300

    config deploy

--- modulo_6_testes_e_deploy/aula_6.5/pratica/hardhat.config.js
+++ modulo_6_testes_e_deploy/aula_6.5/pratica/hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const { TESTNET_DEPLOYER, TESTNET_ADMIN, TESTNET_PAUSER, TESTNET_MINTER } =
+  process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+    amoy: {
+      url: "https://rpc-amoy.polygon.technology",
+      accounts: [
+        `${TESTNET_DEPLOYER}`,
+        `${TESTNET_ADMIN}`,
+        `${TESTNET_PAUSER}`,
+        `${TESTNET_MINTER}`,
+      ],
+    },
+  },
 };
